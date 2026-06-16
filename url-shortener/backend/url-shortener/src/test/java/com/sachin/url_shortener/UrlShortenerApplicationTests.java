f9e6fa82043bc70fda8cb64d7ea098f8f4167283
Sachin <83941937+Sachin20012002@users.noreply.github.com>
2026-06-17 02:40:24 +0530

    V4: Dockerized URL shortener with Nginx load balancing

--- url-shortener/backend/url-shortener/src/test/java/com/sachin/url_shortener/UrlShortenerApplicationTests.java
+++ url-shortener/backend/url-shortener/src/test/java/com/sachin/url_shortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,10 @@
 package com.sachin.url_shortener;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class UrlShortenerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
